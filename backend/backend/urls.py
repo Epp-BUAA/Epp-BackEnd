f7afb3564d64042ae02ffa941f81fa43dbc8df7e
陈俊华 <21373307@buaa.edu.cn>
2024-04-15 22:27:51 +0800

    增加用户信息模块接口

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,19 +24,20 @@ from business.api.userInteraction import like_paper, score_paper, collect_paper
 from business.api import user_info
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/login", login),
-    path("api/signup", signup),
-    path("api/testLogin", testLogin),
-    path("api/logout", logout),
-    path("api/likePaper", like_paper),
-    path("api/scorePaper", score_paper),
-    path("api/collectPaper", collect_paper),
+                  path("admin/", admin.site.urls),
+                  path("api/login", login),
+                  path("api/signup", signup),
+                  path("api/testLogin", testLogin),
+                  path("api/logout", logout),
+                  path("api/likePaper", like_paper),
+                  path("api/scorePaper", score_paper),
+                  path("api/collectPaper", collect_paper),
 
-    # 个人信息模块
-    path("api/userInfo/userInfo", user_info.user_info),
-    path("api/userInfo/avatar", user_info.modify_avatar),
-    path("api/userInfo/collectedPapers", user_info.collected_papers),
-    path("api/userInfo/searchHistory", user_info.search_history),
+                  # 个人信息模块
+                  path("api/userInfo/userInfo", user_info.user_info),
+                  path("api/userInfo/avatar", user_info.modify_avatar),
+                  path("api/userInfo/collectedPapers", user_info.collected_papers),
+                  path("api/userInfo/searchHistory", user_info.search_history),
+                  path("api/userInfo/delSearchHistory", user_info.delete_search_history),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/business/api/user_info.py
+++ backend/business/api/user_info.py
@@ -6,6 +6,7 @@ from django.views.decorators.http import require_http_methods
 import json
 
 from business.models import User
+from business.models import SearchRecord
 from business.utils import req
 
 
@@ -70,28 +71,25 @@ def collected_papers(request):
 
 @require_http_methods('GET')
 def search_history(request):
-    """ 收藏文献列表 """
+    """ 搜索历史列表 """
     username = request.session.get('username')
     user = User.objects.filter(username=username).first()
     if not user:
         return req.fail(msg="请先正确登录")
-    data = {'total': 0, 'papers': []}
-    papers_cnt = 0
-    for paper in user.collected_papers.all():
-        papers_cnt += 1
-        data['papers'].append({
-            "paper_id": paper.paper_id,
-            "title": paper.title,
-            "authors": paper.authors.split(','),
-            "abstract": paper.abstract,
-            "publication_date": paper.publication_date,
-            "journal": paper.journal,
-            "citation_count": paper.citation_count,
-            "read_count": paper.read_count,
-            "like_count": paper.like_count,
-            "collect_count": paper.collect_count,
-            "download_count": paper.download_count,
-            "score": paper.score
+    data = {'total': 0, 'keywords': []}
+    cnt = 0
+    for item in SearchRecord.objects.filter(user_id=user):
+        cnt += 1
+        data['keywords'].append({
+            "search_record_id": item.search_record_id,
+            "keyword": item.keyword,
+            "date": item.date
         })
-    data['total'] = papers_cnt
-    return req.success(data=data, msg='收藏文章列表获取成功')
+    data['total'] = cnt
+    return req.success(data=data, msg='搜索历史记录获取成功')
+
+
+@require_http_methods('POST')
+def delete_search_history(request):
+    """ 删除历史搜索记录 """
+    pass
