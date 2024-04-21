3b7a0aecf93c076a09ecf586ed7f93c7676614bf
MarSeventh <1193267292@qq.com>
2024-04-21 19:09:34 +0800

    fixabug

--- backend/business/api/search.py
+++ backend/business/api/search.py
@@ -9,7 +9,7 @@ from business.models.search_record import SearchRecord, User
 from django.conf import settings
 import datetime
 
-from backend.vector_database.sci_bert_embedding import search_paper_with_query
+from vector_database.sci_bert_embedding import search_paper_with_query
 
 server_ip = '172.17.62.88'
 url = f'http://{server_ip}:8000'
