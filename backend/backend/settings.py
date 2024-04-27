8f1f7e0a64a1e480a2b453d31f772a39998d8887
JingYuChou <zjy262702057@outlook.com>
2024-04-28 00:27:39 +0800

    完成了论文摘要接口的测试

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -175,7 +175,7 @@ USER_READ_MAP_PATH = 'resource/database/users/conversation/read/file_reading_2_t
 PAPERS_PATH = 'resource/database/papers/'  # 数据库论文路径
 BATCH_DOWNLOAD_PATH = 'resource/database/users/batch_download'  # 批量下载文件路径
 
-PAPERS_URL = '/resource/database/papers/'  # 数据库论文本地URL
+PAPERS_URL = 'resource/database/papers/'  # 数据库论文本地URL
 BATCH_DOWNLOAD_URL = '/resource/database/users/batch_download/'  # 批量下载文件本地URL
 USER_DOCUMENTS_URL = '/resource/uploads/users/documents/'  # 用户上传文件本地URL
 CACHE_PATH = '/cache/'  # 缓存路径
--- backend/business/api/summary.py
+++ backend/business/api/summary.py
@@ -22,7 +22,7 @@ def quertGLM(msg: str, history=None) -> str:
     '''
     对chatGLM3-6B发出一次单纯的询问
     '''
-    openai.api_base = f'http://{settings.REMOTE_MODEL_BASE_PATH}/v1'
+    openai.api_base = f'http://{settings.REMOTE_CHATCHAT_GLM3_OPENAI_PATH}/v1'
     openai.api_key = "none"
     history.append({"role": "user", "content": msg})
     response = openai.ChatCompletion.create(
@@ -43,6 +43,8 @@ def generate_summary(request):
         data = json.loads(request.body)
         paper_ids = data.get('paper_id_list')
         username = request.session.get('username')
+        if username is None:
+            username = 'sanyuba'
         from business.models import SummaryReport, User
         user = User.objects.filter(username=username).first()
         report = SummaryReport.objects.create(user_id=user)
@@ -116,15 +118,7 @@ def generate_summary(request):
 
 import os
 import requests
-
-def download_paper(pdf_url, download_dir, filename):
-    # 构建论文的文件名
-    path = os.path.join(download_dir, filename)
-    
-    # 下载论文
-    with requests.get(pdf_url, stream=True) as r, open(path, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=8192):
-            f.write(chunk)
+from business.utils.download_paper import downloadPaper
 
 def create_tmp_knowledge_base(dir : str) -> str:
     '''
@@ -154,45 +148,26 @@ def create_tmp_knowledge_base(dir : str) -> str:
     else:
         return None
     
-    
-def ask_ai(query : str, tmp_kb_id : str):
+def ask_ai_single_paper(payload):
     file_chat_url = f'http://{settings.REMOTE_MODEL_BASE_PATH}/chat/file_chat'
-    payload = json.dumps({
-        "query": query,
-        "knowledge_id": tmp_kb_id,
-        "prompt_name": "deafult"  # 使用历史记录对话模式
-    })
     headers = {
         'Content-Type': 'application/json'
     }
-    response = requests.request("POST", file_chat_url, data=payload, headers=headers, stream=True)
+    response = requests.request("POST", file_chat_url, data=payload, headers=headers, stream=False)
     ai_reply = ""
     origin_docs = []
-    try:
-        for line in response.iter_lines():
-
-            if line:
-                decoded_line = line.decode('utf-8')
-
-                if decoded_line.startswith('id'):
-                    client_id = decoded_line.replace('id: ', '')
-                elif decoded_line.startswith('data'):
-                    data = decoded_line.replace('data: ', '')
-                    data = json.loads(data)
-                    print(data["answer"])
-                    print(data["docs"])
-                    ai_reply += data["answer"]
-                    for doc in data["docs"]:
-                        doc = str(doc).replace("\n", " ")
-                        origin_docs.append(doc)
-
-                    # print(f"Received data (Client ID {client_id}): {data}")
-                elif decoded_line.startswith('event'):
-                    event_type = decoded_line.replace('event: ', '')
-                    # print(f"Event type: {event_type}")
-    finally:
-        response.close()
-    return ai_reply
+    print(response)
+    for line in response.iter_lines():
+        if line:
+            decoded_line = line.decode('utf-8')
+            if decoded_line.startswith('data'):
+                data = decoded_line.replace('data: ', '')
+                data = json.loads(data)
+                ai_reply += data["answer"]
+                for doc in data["docs"]:
+                    doc = str(doc).replace("\n", " ").replace("<span style='color:red'>", "").replace("</span>", "")
+                    origin_docs.append(doc)
+    return ai_reply, origin_docs
     
     
 def create_abstract_report(request):
@@ -200,36 +175,45 @@ def create_abstract_report(request):
     document_id = request_data.get("document_id")
     paper_id = request_data.get("paper_id")
     username = request.session.get('username')
+    print(paper_id)
     if username is None:
         username = 'sanyuba'
     print(username)
     user = User.objects.filter(username=username).first()
     if user is None:
         return fail(msg="请先正确登录")
-    if document_id is not None:
+    if len(document_id) != 0:
         document = UserDocument.objects.get(document_id=document_id)
         # 获取服务器本地的path
         local_path = document.local_path
         content_type = document.format
         title = document.title
-    elif paper_id is not None:
-        p = Paper.objects.filter(document_id=paper_id).first()
+    elif len(paper_id) != 0:
+        p = Paper.objects.filter(paper_id=paper_id).first()
         pdf_url = p.original_url.replace('abs/','pdf/') + '.pdf'
-        local_path = settings.PAPERS_URL + '/' + str(p.paper_id) + '.pdf'
+        local_path = settings.PAPERS_URL  + str(p.paper_id) + '.pdf'
+        print(local_path)
+        print(pdf_url)
         if os.path.exists(local_path) == False:
             # 下载下来
-            download_paper(pdf_url=pdf_url, download_dir=settings.PAPERS_URL, filename=str(p.paper_id) + '.pdf')
+            downloadPaper(url=pdf_url, filename=str(p.paper_id))
         content_type = '.pdf'
         title = p.title
+    print('下载完毕')
 
-    # 创建一段新的filereading对话, 并设置conversation对话路径，创建json文件
-    from business.models import abstract_report
+    from business.models.abstract_report import AbstractReport
     report_path = os.path.join(settings.USER_REPORTS_PATH, title + '.md')
-    ar = abstract_report.objects.create(file_local_path=local_path, report_path=report_path)
+    if os.path.exists(report_path):
+        content = open(report_path, 'r').read()
+        print(content)
+        return success({'summary': content}, msg="生成摘要成功")
+    ar = AbstractReport.objects.create(file_local_path=local_path, report_path=report_path)
     ar.save()
     # 上传到远端服务器, 创建新的临时知识库
     upload_temp_docs_url = f'http://{settings.REMOTE_MODEL_BASE_PATH}/knowledge_base/upload_temp_docs'
     payload = {}
+    local_path = local_path[1:] if local_path.startswith('/') else local_path
+    print(local_path)
     files = [
         ('files', (title + content_type, open(local_path, 'rb'),
                    'application/vnd.openxmlformats-officedocument.presentationml.presentation'))
@@ -237,8 +221,8 @@ def create_abstract_report(request):
     response = requests.post(upload_temp_docs_url, files=files)
 
     # 关闭文件，防止内存泄露
-    for file_tuple in files['files']:
-        file_tuple[1].close()
+    for k, v in files:
+        v[1].close()
     if response.status_code != 200:
         return fail(msg="连接模型服务器失败")
     tmp_kb_id = response.json()['data']['id']
@@ -246,22 +230,74 @@ def create_abstract_report(request):
     # 开始生成摘要
     ## 现状，解决问题，解决方法，实验结果，结论
     summary += '# 摘要报告\n'
+    
+    from business.api.paper_interpret import do_file_chat
+    
+    #### 研究现状
+    
     query_current_situation = '请讲述研究现状部分\n'
-    response_current_situation = ask_ai(query_current_situation, tmp_kb_id)
+    payload_cur_situation = json.dumps({
+        "query": query_current_situation,
+        "knowledge_id": tmp_kb_id,
+        "prompt_name": "default"  # 使用普通对话模式
+    })
+    response_current_situation,_ = ask_ai_single_paper(payload=payload_cur_situation)
+    print(_)
     summary += '## 研究现状\n' + response_current_situation + '\n'
-    query_problem = '请讲讲讲这篇论文解决的问题\n'
-    response_problem = ask_ai(query_problem, tmp_kb_id)
+    
+    #### 解决问题
+    
+    query_problem = '请讲讲这篇论文解决的问题\n'
+    payload_problem = json.dumps({
+        "query": query_problem,
+        "knowledge_id": tmp_kb_id,
+        "prompt_name": "default",  # 使用普通对话模式
+        "score_threshold" : 0.1
+    })
+    response_problem,_ = ask_ai_single_paper(payload=payload_problem)
+    print(_)
     summary += '## 解决问题\n' + response_problem + '\n'
-    query_solution = '请讲讲讲这篇论文的解决方法\n'
-    response_solution = ask_ai(query_solution, tmp_kb_id)
+    
+    #### 解决方法
+    
+    query_solution = '请讲讲这篇论文提出的解决方法\n'
+    payload_solution = json.dumps({
+        "query": query_problem,
+        "knowledge_id": tmp_kb_id,
+        "prompt_name": "default"  # 使用普通对话模式
+    })
+    response_solution,_ = ask_ai_single_paper(payload=payload_solution)
+    print(_)
     summary += '## 解决方法\n' + response_solution + '\n'
-    query_result = '请讲讲讲这篇论文的实验结果\n'
-    response_result = ask_ai(query_result, tmp_kb_id)
+    
+    #### 实验结果
+    
+    query_result = '请讲讲这篇论文实验得到的结果\n'
+    payload_res = json.dumps({
+        "query": query_result,
+        "knowledge_id": tmp_kb_id,
+        "prompt_name": "default"  # 使用普通对话模式
+    })
+    response_result,_ = ask_ai_single_paper(payload=payload_res)
+    print(_)
     summary += '## 实验结果\n' + response_result + '\n'
-    query_conclusion = '请讲讲讲这篇论文的结论\n'
-    response_conclusion = ask_ai(query_conclusion, tmp_kb_id)
+    
+    #### 结论
+    
+    query_conclusion = '请讲讲这篇论文得出的结论\n'
+    payload_conclusion = json.dumps({
+        "query": query_conclusion,
+        "knowledge_id": tmp_kb_id,
+        "prompt_name": "default"  # 使用普通对话模式
+    })
+    response_conclusion,_ = ask_ai_single_paper(payload=payload_conclusion)
+    print(_)
     summary += '## 结论\n' + response_conclusion + '\n'
+    
+    
     # 修改语病，更加通顺
+    print(summary)
+    
     prompt = '这是一篇摘要，请让他更加通顺：\n' + summary
     response = quertGLM(prompt, [])
     print(response)
