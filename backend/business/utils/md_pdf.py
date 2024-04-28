094c73cd0eaa9c60111ded06e721ba02d55f1a6c
JingYuChou <zjy262702057@outlook.com>
2024-04-28 17:08:40 +0800

    pandoc for linux

--- backend/business/utils/md_pdf.py
+++ backend/business/utils/md_pdf.py
@@ -15,7 +15,11 @@ path_wk = settings.LINUX_WKHTMLTOPDF_PATH if is_linux() else settings.WIN_WKHTML
 config = pdfkit.configuration(wkhtmltopdf = path_wk)
 
 def md2pdf(input_filename, output_filename):
-    with open(input_filename, 'r', encoding='utf-8') as f:
-        html_text = markdown(f.read(), output_format='html4')
- 
-    pdfkit.from_string(html_text, output_filename, configuration = config, options={'encoding': 'UTF-8', 'quiet': ''})
+    if is_linux():
+        cmd = f'pandoc {input_filename} -o {output_filename}'
+        os.system(cmd)
+    else:
+        with open(input_filename, 'r', encoding='utf-8') as f:
+            html_text = markdown(f.read(), output_format='html4')
+    
+        pdfkit.from_string(html_text, output_filename, configuration = config, options={'encoding': 'UTF-8', 'quiet': ''})
